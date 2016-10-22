07e86a60ad4404a61d101cf6705d63a4d0667573
StephanieHeffer <stephanie.heffer@gmail.com>
2016-10-22 04:17:04 -0200

    Last atualization about images and videos

--- outubroRosa/js/gallery.js
+++ outubroRosa/js/gallery.js
@@ -1,13 +1,6 @@
       let imagecountmen = 1;
       let totalmen = 5;
 
-    /*  function slidegalleryMen(x){
-        let imagemen = document.getElementById('imgGalMen');
-        imagecountmen = imagecountmen + x;
-        if(imagecountmen > totalmen){ imagecountmen = 1;}
-        if(imagecountmen < 1){imagecountmen = totalmen;}
-        imagemen.src = "imagesmobile/imagem"+ imagecountmen + ".jpg";
-      } */
     window.setInterval(function slidegalleryAmen(){
       let imagemen = document.getElementById('imgGalMen');
       imagecountmen = imagecountmen + 1;
@@ -19,14 +12,7 @@
 
     let imagecountfem = 1;
     let totalfem = 5;
-/*
-          function slidegalleryFem(x){
-            let imagefem = document.getElementById('imgGalFem');
-            imagecountfem = imagecountfem + x;
-            if(imagecountfem > totalfem){ imagecountfem = 1;}
-            if(imagecountfem < 1){imagecountfem = totalfem;}
-            imagefem.src = "imagesmobile/img"+ imagecountfem + ".jpg";
-          } */
+
         window.setInterval(function slidegalleryAfem(){
           let imagefem = document.getElementById('imgGalFem');
           imagecountfem = imagecountfem + 1;
